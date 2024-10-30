b0c926d2a2ed1d2f00e0928fe774d599eca81d1d
Abhishek Shekhar <abhishek123shekhar@gmail.com>
2024-10-30 09:04:39 +0530

    index page customisations

---
+++ user/getFeaturedImages.php
@@ -0,0 +1,12 @@
+<?php
+// Database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "rootremedy";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+
+?>
--- user/js/custom.js
+++ user/js/custom.js
@@ -1,46 +1,20 @@
 // to get current year
 function getYear() {
-    var currentDate = new Date();
-    var currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
+  var currentDate = new Date();
+  var currentYear = currentDate.getFullYear();
+  document.querySelector("#displayYear").innerHTML = currentYear;
 }
 
 getYear();
 
+const navbar = document.querySelector(".custom_nav-container");
 
-// client section owl carousel
-$(".client_owl-carousel").owlCarousel({
-    loop: true,
-    margin: 0,
-    dots: false,
-    nav: true,
-    navText: [],
-    autoplay: true,
-    autoplayHoverPause: true,
-    navText: [
-        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-    ],
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 1
-        },
-        1000: {
-            items: 2
-        }
-    }
+window.addEventListener("scroll", () => {
+  const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+
+  if (scrollPosition > 500) {
+    navbar.classList.add("scrolled-navbar");
+  } else {
+    navbar.classList.remove("scrolled-navbar");
+  }
 });
-
-
-
-/** google_map js **/
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
